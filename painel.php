6d38ec7e292395d74babe95d153a5265e34ebbd4
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2018-10-18 12:38:07 -0300

    Font alterada

--- painel.php
+++ painel.php
@@ -1,7 +1,7 @@
 <header>
 	<img src="img/perfil.png" class="perfil" title="<?php session_start(); echo $_SESSION['usuario']['Nome']; ?>">
 	<nav>
-		<h1>Your Money</h1>
+		<h1 class="titulo">Your Money</h1>
 		<ul>
 			<a href="http://localhost/sistema_financeiro/despesa.php"><li>DESPESA</li></a>
 			<a href="http://localhost/sistema_financeiro/renda.php"><li>RENDA</li></a>
